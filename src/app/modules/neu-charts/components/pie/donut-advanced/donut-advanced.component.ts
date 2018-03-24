c3b5f695b304b72d8c1096b807d6d2d4a629dccb
Prasanth Venkatesan <onlyyprasanth@gmail.com>
2018-03-24 18:09:19 +0530

    [Neu-charts] Removed advanced donut chart options

--- src/app/modules/neu-charts/components/pie/donut-advanced/donut-advanced.component.ts
+++ src/app/modules/neu-charts/components/pie/donut-advanced/donut-advanced.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { colorSets } from '@swimlane/ngx-charts/release/utils';
-import { AdvancedDonutOptions } from '../../../utils/models/advanced-donut-options';
 
 /**
  * Advanced donut component
@@ -14,16 +13,11 @@ import { AdvancedDonutOptions } from '../../../utils/models/advanced-donut-optio
 export class DonutAdvancedComponent implements OnInit {
 
     @Input('data') data: any[] = [];
-    @Input('options') customOptions: AdvancedDonutOptions;
-
-    options: AdvancedDonutOptions = new AdvancedDonutOptions();
 
     colorScheme = colorSets.find(s => s.name == 'forest');
 
     constructor() {}
 
-    ngOnInit() {
-        Object.assign(this.options, this.customOptions);
-    }
+    ngOnInit() {}
 
 }
